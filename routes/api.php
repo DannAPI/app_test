0164a258695a91a5b9d3bb5b105d68264fed7e66
DannAPI <danylo.korotieiev@nure.ua>
2021-11-12 16:07:46 +0200

    Исправлены роуты. Убраны из api, и добавлены в web, с изменениями.

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-          Route::post('add-department', [App\Http\Controllers\DepartmentController::class, 'add_department'])->name('depart');
-          Route::get('all-department', [App\Http\Controllers\DepartmentController::class, 'all_department'])->name('all');
-          Route::delete('delete-department/{id}', [App\Http\Controllers\DepartmentController::class,'delete_department'])->name('delete');
-
-          Route::post('add-user', [App\Http\Controllers\UserController::class, 'add_user'])->name('add');
-          Route::get('all-user', [App\Http\Controllers\UserController::class, 'all_user'])->name('users');
-          Route::get('/user-by-id/{user_by_id}', [App\Http\Controllers\UserController::class, 'user_by_id'])->name('user_by_id') ;
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-//Route::get('/all-department', function () {
-//    return view('departments');
-//});
-//Route::get('/all-user', function () {
-//    return view('users');
-//});
-//Route::get('/user-by-id/{user_by_id}', function () {
-//    return view('users');
-//});
+Route::get('create-user', [App\Http\Controllers\DepartmentController::class, 'allDepartmentForIndex'])->name('all-for-index');
+
+Route::post('add-department', [App\Http\Controllers\DepartmentController::class, 'addDepartment'])->name('depart');
+Route::get('all-department', [App\Http\Controllers\DepartmentController::class, 'allDepartment'])->name('all');
+Route::delete('delete-department/{id}', [App\Http\Controllers\DepartmentController::class,'deleteDepartment'])->name('delete');
+
+Route::post('add-user', [App\Http\Controllers\UserController::class, 'addUser'])->name('add');
+Route::get('all-user', [App\Http\Controllers\UserController::class, 'allUser'])->name('users');
+Route::get('/user-by-id/{user_by_id}', [App\Http\Controllers\UserController::class, 'userId'])->name('user_by_id') ;
+
